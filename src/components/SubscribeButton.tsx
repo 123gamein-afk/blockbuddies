5a6087e999131f29a1a0269c38d8fcf4b44b36d0
AdityaAjithKumar <adityaajith.it@gmail.com>
2025-09-05 12:50:21 +0530

    Fix: Add ExternalLinkIcon import to SubscribeButton

--- src/components/SubscribeButton.tsx
+++ src/components/SubscribeButton.tsx
@@ -1,7 +1,7 @@
 import { useSpring, animated } from '@react-spring/web'
 import { useInView } from 'react-intersection-observer'
 import classNames from 'classnames'
-import { YouTubeIcon, PlayIcon } from '@/components/Icons'
+import { YouTubeIcon, PlayIcon, ExternalLinkIcon } from '@/components/Icons'
 
 interface SubscribeButtonProps {
   channelId: string
